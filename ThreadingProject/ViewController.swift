486b16bf8e0fc544118362d37afd8dae1f352334
Mark Santoro <mark_santoro@comcast.net>
2024-08-24 15:37:36 -0700

    demonstrate delays caused by not threading

--- ThreadingProject/ViewController.swift
+++ ThreadingProject/ViewController.swift
@@ -14,17 +14,40 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
     
     @IBOutlet var tableView: UITableView!
     
+    let urlStrings = ["https://images.unsplash.com/photo-1534970028765-38ce47ef7d8d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFyZ2V8ZW58MHx8MHx8fDA%3D",
+        "https://plus.unsplash.com/premium_photo-1661940781747-5b4026f9ee1f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"]
+    
+    var data = Data()
+    var tracker = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
         
+        let data = try! Data(contentsOf: URL(string: urlStrings[tracker])!)
         
+        imageView.image = UIImage(data: data)
+        
+        self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(changeImage))
         
         
     }
 
+    
+    @objc func changeImage(){
+        if tracker == 0{
+            tracker += 1
+        } else{
+            tracker -= 1
+        }
+        
+        data = try! Data(contentsOf: URL(string: urlStrings[tracker])!)
+        imageView.image = UIImage(data: data)
+        
+
+    }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
