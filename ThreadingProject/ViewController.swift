d9410f185aa7399cb17f873a5d8afc7699e04702
Mark Santoro <mark_santoro@comcast.net>
2024-08-24 15:11:29 -0700

    initial setup

--- ThreadingProject/ViewController.swift
+++ ThreadingProject/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        
+        
     }
 
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Threading Text"
+        return cell
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 25
+    }
+    
+    
 }
 
